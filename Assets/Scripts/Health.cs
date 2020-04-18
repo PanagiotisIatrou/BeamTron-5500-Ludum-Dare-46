951a0d7fa355f18321fc1cfcb13ff8ebb42f66bc
Panagiotis Iatrou <iatrou.panag@gmail.com>
2020-04-18 10:22:54 +0300

    Health Bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    // Singleton
+    private static Health _instance;
+    public static Health Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<Health>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public Slider slider;
+    private int maxHealth = 100;
+    private int health;
+
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
+    public static void SetHealth(int amount)
+    {
+        Instance.health = amount;
+        Instance.health = Mathf.Clamp(Instance.health, 0, Instance.maxHealth);
+        Instance.slider.value = (float)Instance.health / Instance.maxHealth;
+    }
+
+    public static void TakeDamage(int amount)
+    {
+        SetHealth(Instance.health - amount);
+    }
+}
