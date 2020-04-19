7fa9a2d30184761dd484b9c8ca8edf9e45c4e737
Panagiotis Iatrou <iatrou.panag@gmail.com>
2020-04-19 13:13:46 +0300

    Intro + Main menu

---
+++ Assets/Scripts/Intro/IntroText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroText : MonoBehaviour
+{
+    private float speed = 3f;
+    private bool called = false;
+
+    private void Update()
+    {
+        if (transform.position.z > 16f && !called)
+        {
+            called = true;
+            BlackFader.GoToScene("MainMenu", UnityEngine.SceneManagement.LoadSceneMode.Single, 3f);
+        }
+
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuButtonListeners.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonListeners : MonoBehaviour
+{
+    public void OnPlayButtonListener()
+    {
+        BlackFader.GoToScene("Game", UnityEngine.SceneManagement.LoadSceneMode.Single, 1f);
+    }
+}
