16dcb30e66bc2e101f7af3b57642bcdf8fb66389
Panagiotis Iatrou <iatrou.panag@gmail.com>
2020-04-19 17:15:43 +0300

    Year counter + radar sprite

---
+++ Assets/Scripts/Year.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Year : MonoBehaviour
+{
+    public TextMeshProUGUI YearText;
+    private int year = 2025;
+    private float yearTimeMax = 5f; // Length of a year in seconds
+    private float yearTimer = 0f;
+
+    private void Update()
+    {
+        yearTimer += Time.deltaTime;
+        if (yearTimer >= yearTimeMax)
+        {
+            yearTimer = 0f;
+            year++;
+            YearText.SetText("YEAR: " + year);
+        }
+    }
+}
