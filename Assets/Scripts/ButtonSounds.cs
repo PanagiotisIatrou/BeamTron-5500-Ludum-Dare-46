282e8f4aaa55bb8ef0cd69b1028678d07c777a7f
Panagiotis Iatrou <iatrou.panag@gmail.com>
2020-04-20 15:11:13 +0300

    Button sounds

---
+++ Assets/Scripts/ButtonSounds.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSounds : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    public AudioClip HoverClip;
+    public AudioClip ClickClip;
+
+    public void OnPointerEnter(PointerEventData ped)
+    {
+        AudioSource.PlayClipAtPoint(HoverClip, Camera.main.transform.position);
+    }
+
+    public void OnPointerClick(PointerEventData ped)
+    {
+        AudioSource.PlayClipAtPoint(ClickClip, Camera.main.transform.position, 0.7f);
+    }
+}
